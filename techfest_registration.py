73f480553593d9c72063e31be698ada66c0ce2a4
Balacuit <12411842@benilde.edu.ph>
2025-11-06 13:47:52 +0800

    task 2

--- techfest_registration.py
+++ techfest_registration.py
@@ -5,4 +5,14 @@ num_participants = int(input("How many participants will register? "))
 if num_participants <= 0:
     print("Invalid number of participants.")
     exit()  # Stop the program
+participants = []  # List to store participant dictionaries
+for i in range(num_participants):
+    name = input("Enter participant name: ")
+    track = input("Enter chosen track: ")
+    participant = {"name": name, "track": track}
+    participants.append(participant)
 
+# Display registered participants
+print("\nRegistered Participants:")
+for idx, p in enumerate(participants, start=1):
+    print(f"{idx}. {p['name']} - {p['track']}")
