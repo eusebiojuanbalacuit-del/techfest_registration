df9a82de83ee73e5b6f243dc2d7471c1e0a7f7f5
Balacuit <12411842@benilde.edu.ph>
2025-11-06 13:42:01 +0800

    task 1

---
+++ techfest_registration.py
@@ -0,0 +1,8 @@
+print("Welcome to SMIT TechFest!")
+print("Event organized by Ej Balacuit of APPDAET <BTCS2>")  # Replace with your actual name and section
+
+num_participants = int(input("How many participants will register? "))
+if num_participants <= 0:
+    print("Invalid number of participants.")
+    exit()  # Stop the program
+
